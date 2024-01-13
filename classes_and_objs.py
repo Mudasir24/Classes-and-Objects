97e86b60fd52618c5b1e623db8ec6f11c0b9f4c9
Mudasir24 <151111505+Mudasir24@users.noreply.github.com>
2024-01-13 16:59:25 +0530

    Add files via upload

---
+++ classes_and_objs.py
@@ -0,0 +1,26 @@
+# A class named Book with attributes: title, author, and genre
+class Book:\
+
+    # Initializing object attributes
+    def __init__(self,title,author,genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
+
+    # Method to display information about book
+    def display_info(self):
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Genre: {self.genre}")
+
+# Creating instances of the Book class
+my_book1 = Book("The Great Gatsby","F. Scott Fitzgerald","Fiction")
+my_book2 = Book("Atomic Habits","James Clear","Nonfiction")
+
+# Displaying the info of book1
+print("Book:1 ")
+my_book1.display_info()
+
+# Displaying the info of book2
+print("\nBook:2")
+my_book2.display_info()
